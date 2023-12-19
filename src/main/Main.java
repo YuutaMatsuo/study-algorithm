7d3aaa24495b0acf089fb1d96600c455952f1414
user <user@192.168.1.45>
2023-12-19 14:39:39 +0900

    配列を引数にそのまま渡していたために、並び替えの結果が元の配列にも影響していた問題を修正

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,26 +13,26 @@ public class Main {
 
 		// バブルソート
 		BubbleSort bs = new BubbleSort();
-		int[] bubbleSort = bs.bubbleSort(nums);
+		int[] bubbleSort = bs.bubbleSort(coppyArray(nums));
 		// 選択ソート（セレクトソート）
 		SelectionSort ss = new SelectionSort();
-		int[] selectionSort = ss.selectionSort(nums);
+		int[] selectionSort = ss.selectionSort(coppyArray(nums));
 		// 挿入ソート
 		InsertionSort is = new InsertionSort();
-		int[] insertionSort = is.insertionSort(nums);
+		int[] insertionSort = is.insertionSort(coppyArray(nums));
 
 		// 結果出力
 
 		System.out.println("並び替え前");
 		print(nums);
 		System.out.println("\n-------------------------------");
-		System.out.println("バブルソート 経過時間：" + bs.time + "ナノ秒");
+		System.out.printf("%-8s経過時間:%,7dナノ秒\n","バブルソート",bs.time);
 		print(bubbleSort);
 		System.out.println("\n-------------------------------");
-		System.out.println("選択ソート　経過時間：" + bs.time + "ナノ秒");
+		System.out.printf("%-8s経過時間:%,7dナノ秒\n","選択ソート",bs.time);
 		print(selectionSort);
 		System.out.println("\n-------------------------------");
-		System.out.println("挿入ソート 経過時間：" + bs.time + "ナノ秒");
+		System.out.printf("%-8s経過時間:%,7dナノ秒\n","挿入ソート",bs.time);
 		print(insertionSort);
 	}
 
@@ -41,5 +41,17 @@ public class Main {
 		for (int num : nums) {
 			System.out.print(num + " ");
 		}
+	}
+	
+	// 配列を複製し戻り値として返す
+	// ※配列は参照型の為、並び替えメソッドの引数にそのまま渡してしまうと
+	// もとの配列を参照して書き換えてしまうため、必ずこのメソッドを呼んで複製する事
+	
+	public static int[] coppyArray(int[] nums) {
+		int[] num = new int[nums.length];
+		for(int i = 0; i < nums.length; i++) {
+			num[i] = nums[i];
+		}
+		return num;
 	}
 }
