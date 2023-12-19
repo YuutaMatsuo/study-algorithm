8faf1b2c8190be812baf3e86fab475f56b72d324
user <user@192.168.1.45>
2023-12-19 15:45:32 +0900

    配列を生成するメソッドを追加

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,57 +1,66 @@
 package main;
 
+import java.util.Random;
+
 import sort.BubbleSort;
 import sort.InsertionSort;
 import sort.SelectionSort;
 
 public class Main {
 	public static void main(String[] args) {
-		// ランダムに数値が入ったしたい配列
-		final int[] nums = { 2, 6, 1, 4, 3, 7, 5, 9, 8 };
 
 		// 並び替え処理
 
 		// バブルソート
 		BubbleSort bs = new BubbleSort();
-		int[] bubbleSort = bs.bubbleSort(coppyArray(nums));
+		int[] bubbleSort = bs.bubbleSort(createArray(10000));
 		// 選択ソート（セレクトソート）
 		SelectionSort ss = new SelectionSort();
-		int[] selectionSort = ss.selectionSort(coppyArray(nums));
+		int[] selectionSort = ss.selectionSort(createArray(10000));
 		// 挿入ソート
 		InsertionSort is = new InsertionSort();
-		int[] insertionSort = is.insertionSort(coppyArray(nums));
+		int[] insertionSort = is.insertionSort(createArray(10000));
 
 		// 結果出力
 
-		System.out.println("並び替え前");
-		print(nums);
-		System.out.println("\n-------------------------------");
-		System.out.printf("%-8s経過時間:%,7dナノ秒\n","バブルソート",bs.time);
+		System.out.printf("%-8s経過時間:%,10dミリ秒\n", "バブルソート", bs.time);
 		print(bubbleSort);
-		System.out.println("\n-------------------------------");
-		System.out.printf("%-8s経過時間:%,7dナノ秒\n","選択ソート",bs.time);
+		System.out.println("-----------------------------------------\n");
+		System.out.printf("%-8s経過時間:%,10dミリ秒\n", "選択ソート", bs.time);
 		print(selectionSort);
-		System.out.println("\n-------------------------------");
-		System.out.printf("%-8s経過時間:%,7dナノ秒\n","挿入ソート",bs.time);
+		System.out.println("-----------------------------------------\n");
+		System.out.printf("%-8s経過時間:%,10dミリ秒\n", "挿入ソート", bs.time);
 		print(insertionSort);
+		System.out.println("-----------------------------------------\n");
 	}
 
 	// 配列を出力する
 	public static void print(int[] nums) {
+		StringBuilder sb = new StringBuilder();
 		for (int num : nums) {
-			System.out.print(num + " ");
+			sb.append(num + " ");
 		}
+		System.out.println(sb);
 	}
-	
+
 	// 配列を複製し戻り値として返す
 	// ※配列は参照型の為、並び替えメソッドの引数にそのまま渡してしまうと
 	// もとの配列を参照して書き換えてしまうため、必ずこのメソッドを呼んで複製する事
-	
 	public static int[] coppyArray(int[] nums) {
 		int[] num = new int[nums.length];
-		for(int i = 0; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			num[i] = nums[i];
 		}
 		return num;
+	}
+
+	// 配列を生成する
+	public static int[] createArray(int num) {
+		Random random = new Random();
+		int[] nums = new int[num];
+		for (int i = 0; i < num; i++) {
+			nums[i] = random.nextInt(100);
+		}
+		return nums;
 	}
 }
--- src/main/Timer.java
+++ src/main/Timer.java
@@ -4,14 +4,17 @@ public class Timer {
 	long startTime;
 	long endTime;
 
+	// 開始時間を記録
 	public void start() {
-		this.startTime = System.nanoTime();
+		this.startTime = System.currentTimeMillis();
 	}
 
+	// 終了時間を記録
 	public void end() {
-		this.endTime = System.nanoTime();
+		this.endTime = System.currentTimeMillis();
 	}
 
+	// 終了時間 - 開始時間を計算し経過時間を戻り値として返す
 	public long calc() {
 		return (this.endTime - this.startTime);
 	}
